227b901f20c426c49f7b6096236279f7f45c264c
Claudia Pierson <c172@umbc.edu>
2020-05-11 03:18:11 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,12 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-class Kid 
-  include Dance 
+class Kid
+  include Dance
   extend MetaDancing
-  attr_accessor :name 
-  @name = name 
-  
+  attr_accessor :name
+
   def initialize(name)
-    @name = name 
-  end 
-end 
+    @name = name
+  end
+end
