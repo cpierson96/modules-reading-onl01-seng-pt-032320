6db0e4f42e829b29d6c80364e6c532c8ea178f2b
Claudia Pierson <c172@umbc.edu>
2020-05-11 03:23:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-class Dancer <FancyDance
-  extend FancyDance::ClassMethods
+class Dancer
+  # include Dance
+  # extend MetaDancing
   include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
